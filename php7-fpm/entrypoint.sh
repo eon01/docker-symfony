7cf37d4da50fa1884ec3c72c89dd95c16c04d22b
Aymen EL AMRI <amri.aymen@gmail.com>
2017-06-01 16:55:06 +0200

    add cd to dir and force create var

--- php7-fpm/entrypoint.sh
+++ php7-fpm/entrypoint.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 set -e
 
+cd /var/www/symfony
+mkdir -p var
+
+ls -lrth
+
 HTTPDUSER=$(ps axo user,comm | grep -E '[a]pache|[h]ttpd|[_]www|[w]ww-data|[n]ginx' | grep -v root | head -1 | cut -d\  -f1)
 
 setfacl -R -m u:"$HTTPDUSER":rwX -m u:$(whoami):rwX var
